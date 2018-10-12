35e126680a78812682930e1877e177e3de425621
MBBe <43997306+MBBe@users.noreply.github.com>
2018-10-12 12:10:36 +0100

    Latin Prototype
    another prototype for the TNA Latin website

---
+++ manasseh-prototype/Lesson_Template.php
@@ -0,0 +1,46 @@
+<!--/**
+ * Created by PhpStorm.
+ * User: a-mboyd
+ * Date: 27/09/2017
+ * Time: 11:33
+ */-->
+<!DOCTYPE html>
+<head>
+    <title>Lesson Template</title>
+    <?php
+    /*add includes for the document head and header php */
+    include "./Template-Includes/d-document-header.php";
+    include "./Template-Includes/d-page-header.php";
+    include "./Template-Includes/d-breadcrumbs";
+    ?>
+</head>
+<body>
+<?php
+/* add include for breadcrumbs here */
+;
+?>
+<main id="primary" role="main" class="content-area">
+    <div class="container">
+        <?php
+        /*add includes for banner and blurb here */
+        include "./Template-Includes/d-lessonbanner.php";
+        include "./Content-Includes/d-lesson-content.php";
+        include "./Template-Includes/d-other-resources-banner.php";
+        ?>
+        <?php
+        /*include resoucres banner here*/
+        /*include "./Landingpage_Includes/d-beginners-latin-banner.php";*/
+        ?>
+    </div>
+</main>
+<?php
+/*add include for the newsletter */
+include "./Template-Includes/d-newsletter-signup.php";
+?>
+</body>
+<footer id="footer" class="breather-top-bottom" role="contentinfo">
+    <?php
+    /* add include for footer here*/
+    include "./Template-Includes/d-footer.php";
+    ?>
+</footer>
